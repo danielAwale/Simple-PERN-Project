950b2d876f53456f5f6d5425c4cdf9727f391ae0
danielAwale <danielmadhukarawale@gmail.com>
2022-10-29 09:49:34 -0700

    contain a function to pick up the response for our localhost

--- client/src/components/InputTodo.js
+++ client/src/components/InputTodo.js
@@ -1,11 +1,30 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
 const InputTodo = () => {
+  const [description, setDescription] = useState("");
+
+  const onSubmitForm = async e => {
+    e.preventDefault();
+    try {
+      const body = { description };
+      const response = fetch("http://localhost:5000/todos", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body)
+      });
+    } catch (err) {
+      console.error(err.message)
+    }
+  }
   return (
     <Fragment>
       <h1 className="text-center mt-5">Input Todo</h1>
-      <form className="d-flex">
-        <input type="text" className="form-control" />
+      <form className="d-flex mt-5">
+        <input
+          type="text"
+          className="form-control"
+          value={description}
+          onChange={e => setDescription(e.target.value)} />
         <button className="btn btn-success">Add</button>
       </form>
     </Fragment>
