902a223a4d37336046bdfa469f6a76d441d3a17b
danielAwale <danielmadhukarawale@gmail.com>
2022-10-29 08:40:26 -0700

    contain a router to a get a specific todo aka :id

--- server/index.js
+++ server/index.js
@@ -39,6 +39,16 @@ app.get("/todos", async (req, res) => {
 })
 
 // get a todo
+app.get("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [id])
+    res.json(res.json(todo.rows[0]))
+
+  } catch (err) {
+    console.error(err.message)
+  }
+})
 
 // update a todo
 
