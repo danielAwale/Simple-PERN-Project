b09e2f9a1683e2e98369888a3b3f215058eb7b3d
danielAwale <danielmadhukarawale@gmail.com>
2022-10-29 08:50:46 -0700

    contain a delete route for the project: RESTful API complete

--- server/index.js
+++ server/index.js
@@ -65,6 +65,17 @@ app.put("/todos/:id", async (req, res) => {
 })
 
 // delete a todo
+app.delete("/todo/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [id])
+    res.json("todo was deleted")
+
+  }
+  catch (err) {
+    console.error(err.message)
+  }
+})
 
 app.listen(5000, () => {
   console.log("server has started on port 5000");
