22f95ef0a1fb3700c75d1699a6e5d69d20cdb1eb
danielAwale <danielmadhukarawale@gmail.com>
2022-10-29 08:46:44 -0700

    contain a route for the updating a todo

--- server/index.js
+++ server/index.js
@@ -51,6 +51,18 @@ app.get("/todos/:id", async (req, res) => {
 })
 
 // update a todo
+app.put("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+    const updateTodo = await pool.query(
+      "UPDATE todo SET description = $1 WHERE todo_id = $2",
+      [description, id])
+    res.json("todo was updated")
+  } catch (err) {
+    console.error(err.message);
+  }
+})
 
 // delete a todo
 
