303fda1aeb963f23cd4e0d4c9bcf8e2fe01c7d50
danielAwale <danielmadhukarawale@gmail.com>
2022-10-30 16:26:27 -0700

    try on debuggin some issues related to useEffects

--- client/src/components/ListTodo.js
+++ client/src/components/ListTodo.js
@@ -2,7 +2,21 @@ import React, { Fragment, useState, useEffect } from 'react'
 
 const ListTodo = () => {
 
-  const [todos, setTodos] = useState("")
+  const [todos, setTodos] = useState("");
+
+  const deleteTodo = async (id) => {
+    try {
+      const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`, {
+        method: "DELETE"
+      });
+
+      console.log(deleteTodo)
+
+    } catch (err) {
+      console.log(err.message)
+
+    }
+  }
   const getTodos = async () => {
     try {
       const response = await fetch("http://localhost:5000/todos");
@@ -18,9 +32,11 @@ const ListTodo = () => {
   useEffect(() => {
     getTodos();
   }, [])
+
+  console.log(todos)
   return (
     <Fragment>
-      <table className="table mt-5 text-centre">
+      <table className="table mt-5 text-center">
         <thead>
           <tr>
             <th>Description</th>
@@ -29,18 +45,11 @@ const ListTodo = () => {
           </tr>
         </thead>
         <tbody>
-          {/* 
-          <tr>
-            <td>John</td>
-            <td>Doe</td>
-            <td>john@example.com</td>
-          </tr> 
-        */}
           {todos.map(todo => (
-            <tr>
+            <tr key={todo.todo_id}>
               <td>{todo.description}</td>
               <td>Edit</td>
-              <td>Delete</td>
+              <td><button className="btn btn-danger" onClick={() => deleteTodo(todo.todo_id)}>Delete</button></td>
             </tr>
           ))}
         </tbody>
