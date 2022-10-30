3e2d1ee19dd29b72e0d8d2175743713f22af1287
danielAwale <danielmadhukarawale@gmail.com>
2022-10-30 13:59:37 -0700

    will continue to work

--- client/src/components/InputTodo.js
+++ client/src/components/InputTodo.js
@@ -7,7 +7,7 @@ const InputTodo = () => {
     e.preventDefault();
     try {
       const body = { description };
-      const response = fetch("http://localhost:5000/todos", {
+      const response = await fetch("http://localhost:5000/todos", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(body)
@@ -19,7 +19,7 @@ const InputTodo = () => {
   return (
     <Fragment>
       <h1 className="text-center mt-5">Input Todo</h1>
-      <form className="d-flex mt-5">
+      <form className="d-flex mt-5" onSubmit={onSubmitForm}>
         <input
           type="text"
           className="form-control"
