34b74447ebb48406c910f17641484f4c7f23a2b6
danielAwale <danielmadhukarawale@gmail.com>
2022-10-30 17:19:53 -0700

    fix the modal bug with the id

--- client/src/components/EditTodo.js
+++ client/src/components/EditTodo.js
@@ -1,14 +1,15 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 
 const EditTodo = ({ todo }) => {
+  const [description, setDescription] = useState(todo.description)
   console.log(todo)
   return (
     <>
-      <button type="button" className="btn btn-warning" data-toggle="modal" data-target="#myModal">
+      <button type="button" className="btn btn-warning" data-toggle="modal" data-target={`#id${todo.todo_id}`}>
         Edit
       </button>
 
-      <div className="modal" id="myModal">
+      <div className="modal" id={`id${todo.todo_id}`}>
         <div className="modal-dialog">
           <div className="modal-content">
 
@@ -18,7 +19,7 @@ const EditTodo = ({ todo }) => {
             </div>
 
             <div className="modal-body">
-              <input type="text" className='form-control' />
+              <input type="text" className='form-control' value={description} />
             </div>
 
             <div className="modal-footer">
