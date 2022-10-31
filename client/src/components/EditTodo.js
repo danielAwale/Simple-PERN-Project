3824e5be13729370c3babafabe41217d7750b316
danielAwale <danielmadhukarawale@gmail.com>
2022-10-30 17:12:29 -0700

    contain props

--- client/src/components/EditTodo.js
+++ client/src/components/EditTodo.js
@@ -1,31 +1,29 @@
 import React, { Fragment } from 'react'
 
-const EditTodo = () => {
+const EditTodo = ({ todo }) => {
+  console.log(todo)
   return (
     <>
-      <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#myModal">
+      <button type="button" className="btn btn-warning" data-toggle="modal" data-target="#myModal">
         Edit
       </button>
 
-      <div class="modal" id="myModal">
-        <div class="modal-dialog">
-          <div class="modal-content">
+      <div className="modal" id="myModal">
+        <div className="modal-dialog">
+          <div className="modal-content">
 
-            <div class="modal-header">
-              <h4 class="modal-title">Edit Todo</h4>
-              <button type="button" class="close" data-dismiss="modal">&times;</button>
+            <div className="modal-header">
+              <h4 className="modal-title">Edit Todo</h4>
+              <button type="button" className="close" data-dismiss="modal">&times;</button>
             </div>
 
-            <div class="modal-body">
+            <div className="modal-body">
               <input type="text" className='form-control' />
             </div>
 
-            <div class="modal-footer">
-              <button type="button" class="btn btn-warning" data-dismiss="modal">Edit</button>
-            </div>
-
-            <div class="modal-footer">
-              <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-warning" data-dismiss="modal">Edit</button>
+              <button type="button" className="btn btn-danger" data-dismiss="modal">Close</button>
             </div>
 
           </div>
